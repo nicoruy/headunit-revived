1f1ad85fd0dda9144a8bf2c0d3a92a5a1c84c09e
ars <andre.rinas@netempire.software>
2026-02-13 09:10:45 +0100

    make the activity reload appear on main too, if you change the language

--- app/src/main/java/com/andrerinas/headunitrevived/app/BaseActivity.kt
+++ app/src/main/java/com/andrerinas/headunitrevived/app/BaseActivity.kt
@@ -1,8 +1,10 @@
 package com.andrerinas.headunitrevived.app
 
 import android.content.Context
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.andrerinas.headunitrevived.utils.LocaleHelper
+import com.andrerinas.headunitrevived.utils.Settings
 
 /**
  * Base Activity that handles app language configuration.
@@ -10,7 +12,23 @@ import com.andrerinas.headunitrevived.utils.LocaleHelper
  */
 open class BaseActivity : AppCompatActivity() {
 
+    private var currentLanguage: String? = null
+
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(LocaleHelper.wrapContext(newBase))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        currentLanguage = Settings(this).appLanguage
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val settings = Settings(this)
+        if (currentLanguage != settings.appLanguage) {
+            // Language changed while we were in the background
+            recreate()
+        }
     }
 }
